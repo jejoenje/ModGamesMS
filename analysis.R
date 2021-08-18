b18828600b573c05700b338690eb1e96d4dff91e
jejoenje <jeroenminderman@gmail.com>
2021-08-18 20:44:10 +0100

    Forgot to save, as previous commit

--- analysis.R
+++ analysis.R
@@ -166,13 +166,14 @@ mtext("Time step", side = 1, outer = T, line = 2.5, cex = CEX.MTEXT)
 mtext("True population size", side = 2, outer = T, line = 2.5, cex =CEX.MTEXT)
 
 ### Calculate summary statistic for given cost type over time steps:
-cost_summary = function(dat, ov, cost = "cull_cost", type) {
+cost_summary = function(dat, ov, cost, type) {
         
         if(!(cost %in% names(dat))) stop("'cost' specified not present in 'dat'.")
         if(!(type %in% c("mean","median","q25","q75","q025","q975"))) stop("invalid 'type' specified .")
         
         d = dat[(dat$ownership_var==ov & dat$t>5),c("t",cost)]
         d[,cost] = (d[,cost]-10)*10
+        d$t = factor(d$t)
         
         if(type == "mean") return(tapply(d[,cost],d$t,function(x) mean(x, na.rm=T)))
         if(type == "median") return(tapply(d[,cost],d$t,function(x) median(x, na.rm=T)))
@@ -182,56 +183,6 @@ cost_summary = function(dat, ov, cost = "cull_cost", type) {
         if(type == "q975") return(tapply(d[,cost],d$t,function(x) quantile(x, probs = 0.975, na.rm=T)))
 
 }
-# 
-# c_cost000 = gdata_run[(gdata_run$ownership_var==0 & gdata_run$t>5),c("t","cull_cost")]
-# c_cost025 = gdata_run[(gdata_run$ownership_var==0.25 & gdata_run$t>5),c("t","cull_cost")]
-# c_cost050 = gdata_run[(gdata_run$ownership_var==0.5 & gdata_run$t>5),c("t","cull_cost")]
-# c_cost000$cull_cost = (c_cost000$cull_cost-10)*10
-# c_cost025$cull_cost = (c_cost025$cull_cost-10)*10
-# c_cost050$cull_cost = (c_cost050$cull_cost-10)*10
-# 
-# s_cost000 = gdata_run[(gdata_run$ownership_var==0 & gdata_run$t>5),c("t","scare_cost")]
-# s_cost025 = gdata_run[(gdata_run$ownership_var==0.25 & gdata_run$t>5),c("t","scare_cost")]
-# s_cost050 = gdata_run[(gdata_run$ownership_var==0.5 & gdata_run$t>5),c("t","scare_cost")]
-# s_cost000$scare_cost = (s_cost000$scare_cost-10)*10
-# s_cost025$scare_cost = (s_cost025$scare_cost-10)*10
-# s_cost050$scare_cost = (s_cost050$scare_cost-10)*10
-# 
-# c_cost000_median = tapply(c_cost000$cull_cost, c_cost000$t, function(x) median(x,na.rm=T))
-# c_cost000_l1 = tapply(c_cost000$cull_cost, c_cost000$t, function(x) quantile(x,prob = 0.25, na.rm=T))
-# c_cost000_u1 = tapply(c_cost000$cull_cost, c_cost000$t, function(x) quantile(x,prob = 0.75, na.rm=T))
-# c_cost000_l2 = tapply(c_cost000$cull_cost, c_cost000$t, function(x) quantile(x,prob = 0.025, na.rm=T))
-# c_cost000_u2 = tapply(c_cost000$cull_cost, c_cost000$t, function(x) quantile(x,prob = 0.975, na.rm=T))
-# 
-# c_cost025_median = tapply(c_cost025$cull_cost, c_cost025$t, function(x) median(x,na.rm=T))
-# c_cost025_l1 = tapply(c_cost025$cull_cost, c_cost025$t, function(x) quantile(x,prob = 0.25, na.rm=T))
-# c_cost025_u1 = tapply(c_cost025$cull_cost, c_cost025$t, function(x) quantile(x,prob = 0.75, na.rm=T))
-# c_cost025_l2 = tapply(c_cost025$cull_cost, c_cost025$t, function(x) quantile(x,prob = 0.025, na.rm=T))
-# c_cost025_u2 = tapply(c_cost025$cull_cost, c_cost025$t, function(x) quantile(x,prob = 0.975, na.rm=T))
-# 
-# c_cost050_median = tapply(c_cost050$cull_cost, c_cost050$t, function(x) median(x,na.rm=T))
-# c_cost050_l1 = tapply(c_cost050$cull_cost, c_cost050$t, function(x) quantile(x,prob = 0.25, na.rm=T))
-# c_cost050_u1 = tapply(c_cost050$cull_cost, c_cost050$t, function(x) quantile(x,prob = 0.75, na.rm=T))
-# c_cost050_l2 = tapply(c_cost050$cull_cost, c_cost050$t, function(x) quantile(x,prob = 0.025, na.rm=T))
-# c_cost050_u2 = tapply(c_cost050$cull_cost, c_cost050$t, function(x) quantile(x,prob = 0.975, na.rm=T))
-# 
-# s_cost000_median = tapply(s_cost000$scare_cost, s_cost000$t, function(x) median(x,na.rm=T))
-# s_cost000_l1 = tapply(s_cost000$scare_cost, s_cost000$t, function(x) quantile(x,prob = 0.25, na.rm=T))
-# s_cost000_u1 = tapply(s_cost000$scare_cost, s_cost000$t, function(x) quantile(x,prob = 0.75, na.rm=T))
-# s_cost000_l2 = tapply(s_cost000$scare_cost, s_cost000$t, function(x) quantile(x,prob = 0.025, na.rm=T))
-# s_cost000_u2 = tapply(s_cost000$scare_cost, s_cost000$t, function(x) quantile(x,prob = 0.975, na.rm=T))
-# 
-# s_cost025_median = tapply(s_cost025$scare_cost, s_cost025$t, function(x) median(x,na.rm=T))
-# s_cost025_l1 = tapply(s_cost025$scare_cost, s_cost025$t, function(x) quantile(x,prob = 0.25, na.rm=T))
-# s_cost025_u1 = tapply(s_cost025$scare_cost, s_cost025$t, function(x) quantile(x,prob = 0.75, na.rm=T))
-# s_cost025_l2 = tapply(s_cost025$scare_cost, s_cost025$t, function(x) quantile(x,prob = 0.025, na.rm=T))
-# s_cost025_u2 = tapply(s_cost025$scare_cost, s_cost025$t, function(x) quantile(x,prob = 0.975, na.rm=T))
-# 
-# s_cost050_median = tapply(s_cost050$scare_cost, s_cost050$t, function(x) median(x,na.rm=T))
-# s_cost050_l1 = tapply(s_cost050$scare_cost, s_cost050$t, function(x) quantile(x,prob = 0.25, na.rm=T))
-# s_cost050_u1 = tapply(s_cost050$scare_cost, s_cost050$t, function(x) quantile(x,prob = 0.75, na.rm=T))
-# s_cost050_l2 = tapply(s_cost050$scare_cost, s_cost050$t, function(x) quantile(x,prob = 0.025, na.rm=T))
-# s_cost050_u2 = tapply(s_cost050$scare_cost, s_cost050$t, function(x) quantile(x,prob = 0.975, na.rm=T))
 
 par(mfrow =c(2,3))
 XLAB = "Time step"
@@ -257,8 +208,10 @@ for(i in 1:6) {
              type = "n", ylab = "", xlab = XLAB, cex.axis = CEX.AXIS, cex.lab = CEX.LAB, cex.main = CEX.MAIN, yaxt = "n", xaxt = "n")
         if(aytype[i]) { axis(2, labels = TRUE, cex.axis = CEX.AXIS) } else { axis(2, labels = FALSE) }
         if(axtype[i]) { axis(1, labels = TRUE, cex.axis = CEX.AXIS) } else { axis(1, labels = FALSE) }
+        if(i==1) mtext("Hunting cost", side = 2, line = 3, cex = CEX.MTEXT)
+        if(i==4) mtext("Scaring cost", side = 2, line = 3, cex = CEX.MTEXT)
         
-        if(i<4) { mtext(bquote(o[v] ~ = ~ .(otype[i])), side=3, line =1.5, cex = CEX.MTEXT) }
+        if(i<4) { mtext(bquote(o[v] ~ "=" ~ .(otype[i])), side=3, line =1.5, cex = CEX.MTEXT) }
         polygon(c(rev(6:24),6:24), 
                 c(rev(cost_summary(gdata_run, ov=otype[i], cost=ptype[i], type="q025")[1:19]),
                   cost_summary(gdata_run, ov=otype[i], cost=ptype[i],type="q975")[1:19]), col = polycols[i,1], border = NA)
@@ -266,71 +219,12 @@ for(i in 1:6) {
                 c(rev(cost_summary(gdata_run, ov = otype[i], cost=ptype[i],type="q25")[1:19]),
                   cost_summary(gdata_run, ov = otype[i], cost=ptype[i],type="q75")[1:19]), col = polycols[i,2], border = NA)
         
-        lines(6:24, cost_summary(gdata_run, ov = otype[i], cost=ptype[i],type="mean")[1:19], lwd = 2)
+        lines(6:24, cost_summary(gdata_run, ov = otype[i], cost=ptype[i],type="median")[1:19], lwd = 2)
         lines(6:24, cost_summary(gdata_run, ov = otype[i], cost=ptype[i],type="q25")[1:19], lwd = 1)
         lines(6:24, cost_summary(gdata_run, ov = otype[i], cost=ptype[i],type="q75")[1:19], lwd = 1)
         lines(6:24, cost_summary(gdata_run, ov = otype[i], cost=ptype[i],type="q025")[1:19], lwd = 0.75)
         lines(6:24, cost_summary(gdata_run, ov = otype[i], cost=ptype[i],type="q975")[1:19], lwd = 0.75)
         
 }
-
-expression(paste(" ",o[v] ,"= "))
-
-
-plot(c(1,1), xlim = c(1,25), ylim = c(0,1000), type = "n", ylab = "", xlab = XLAB, cex.axis = CEX.AXIS, cex.lab = CEX.LAB, cex.main = CEX.MAIN)
-mtext(expression("o"[v]*" = 0"), side=3, line =1.5, cex = CEX.MTEXT)
-polygon(c(rev(6:24),6:24), 
-        c(rev(cost_summary(gdata_run, ov=0, cost="cull_cost", type="q025")[1:19]),
-          cost_summary(gdata_run, ov=0, cost="cull_cost",type="q975")[1:19]), col = "#e6550d", border = NA)
-polygon(c(rev(6:24),6:24), 
-        c(rev(cost_summary(gdata_run, ov = 0, cost="cull_cost",type="q25")[1:19]),
-          cost_summary(gdata_run, ov = 0, cost="cull_cost",type="q75")[1:19]), col = "#fdae6b", border = NA)
-
-lines(6:24, cost_summary(gdata_run, ov = 0, cost="cull_cost",type="median")[1:19], lwd = 2)
-lines(6:24, cost_summary(gdata_run, ov = 0, cost="cull_cost",type="q25")[1:19], lwd = 1)
-lines(6:24, cost_summary(gdata_run, ov = 0, cost="cull_cost",type="q75")[1:19], lwd = 1)
-lines(6:24, cost_summary(gdata_run, ov = 0, cost="cull_cost",type="q025")[1:19], lwd = 0.75)
-lines(6:24, cost_summary(gdata_run, ov = 0, cost="cull_cost",type="q975")[1:19], lwd = 0.75)
-
-plot(c(1,1), xlim = c(1,25), ylim = c(0,1000), type = "n", yaxt="n", ylab = "", xlab = XLAB, cex.axis = CEX.AXIS, cex.lab = CEX.LAB)
-mtext(expression("o"[v]*" = 0.25"), side=3, line =1.5, cex = CEX.MTEXT)
-polygon(c(rev(6:24),6:24), 
-        c(rev(cost_summary(gdata_run, ov=0.25, cost="cull_cost", type="q025")[1:19]),
-          cost_summary(gdata_run, ov=0.25, cost="cull_cost", type="q975")[1:19]), col = "#e6550d", border = NA)
-polygon(c(rev(6:24),6:24), 
-        c(rev(cost_summary(gdata_run, ov=0.25, cost="cull_cost", type="q25")[1:19]),
-          cost_summary(gdata_run, ov=0.25, cost="cull_cost", type="q75")[1:19]), col = "#fdae6b", border = NA)
-lines(6:24, c_cost025_median[1:19], lwd = 2)
-lines(6:24, c_cost025_l1[1:19], lwd = 1)
-lines(6:24, c_cost025_u1[1:19], lwd = 1)
-lines(6:24, c_cost025_l2[1:19], lwd = 0.75)
-lines(6:24, c_cost025_u2[1:19], lwd = 0.75)
-
-plot(c(1,1), xlim = c(1,25), ylim = c(0,1000), type = "n", yaxt="n", ylab = "", xlab = XLAB, cex.axis = CEX.AXIS, cex.lab = CEX.LAB)
-mtext(expression("o"[v]*" = 0.5"), side=3, line =1.5, cex = CEX.MTEXT)
-polygon(c(rev(6:24),6:24), 
-        c(rev(c_cost050_l2[1:19]),
-          c_cost050_u2[1:19]), col = "#e6550d", border = NA)
-polygon(c(rev(6:24),6:24), 
-        c(rev(c_cost050_l1[1:19]),
-          c_cost050_u1[1:19]), col = "#fdae6b", border = NA)
-lines(6:24, c_cost050_median[1:19], lwd = 2)
-lines(6:24, c_cost050_l1[1:19], lwd = 1)
-lines(6:24, c_cost050_u1[1:19], lwd = 1)
-lines(6:24, c_cost050_l2[1:19], lwd = 0.75)
-lines(6:24, c_cost050_u2[1:19], lwd = 0.75)
-
-
-plot(c(1,1), xlim = c(1,25), ylim = c(0,1000), type = "n", ylab = "", xlab = XLAB, cex.axis = CEX.AXIS, cex.lab = CEX.LAB, cex.main = CEX.MAIN)
-polygon(c(rev(6:24),6:24), c(rev(s_cost000_l2[1:19]),s_cost000_u2[1:19]), col = "#e6550d", border = NA)
-polygon(c(rev(6:24),6:24), c(rev(s_cost000_l1[1:19]),s_cost000_u1[1:19]), col = "#fdae6b", border = NA)
-lines(6:24, s_cost000_median[1:19], lwd = 2)
-lines(6:24, s_cost000_l1[1:19], lwd = 1)
-lines(6:24, s_cost000_u1[1:19], lwd = 1)
-lines(6:24, s_cost000_l2[1:19], lwd = 0.75)
-lines(6:24, s_cost000_u2[1:19], lwd = 0.75)
-
-
 mtext("Time step", side = 1, outer = T, line = 2.5, cex = CEX.MTEXT)
-mtext("Hunting cost", side = 2, outer = T, line = 2.5, cex =CEX.MTEXT)
 
