b610993e3b1da299dfe2093ef5d09b83c37da375
jejoenje <jeroenminderman@gmail.com>
2021-08-19 13:05:09 +0100

    Completed illustrative results text, added in main figures. Adapted template to control figure caption style appropriately.

--- analysis.R
+++ analysis.R
@@ -34,7 +34,6 @@ run = droplevels(run)
 run = run[run$steps>6,]
 
 ### Total number of sessions with at least some player interaction:
-nrow(run)
 total_valid = nrow(run); total_valid
 ### Of this many unique player names:
 total_valid_unames = nlevels(run$player); total_valid_unames
@@ -91,6 +90,11 @@ gdata_run$extinct = run$extinct[match(gdata_run$id, run$id)]
 start_date = format(min(run$startTime),"%d %B %Y"); start_date
 end_date = format(max(run$startTime),"%d %B %Y"); end_date
 
+### Play duration summaries:
+mean_duration = round(as.numeric(mean(difftime(run$endTime, run$startTime, units = "mins"))),1)
+min_duration = round(as.numeric(min(difftime(run$endTime, run$startTime, units = "mins"))),1)
+max_duration = round(as.numeric(max(difftime(run$endTime, run$startTime, units = "mins"))),1)
+
 ### Number of sessions played:
 n_sessions = nrow(run); n_sessions
 
@@ -100,131 +104,22 @@ n_players = nlevels(factor(run$player)); n_players
 ### Number/percentages of sessions with each ownership_var:
 # 0
 as.numeric(names(table(run$ownership_var)[1])) 
-as.numeric(table(run$ownership_var)[1])
-round(as.numeric(table(run$ownership_var)[1])/n_sessions,2)
+n_ov0 = as.numeric(table(run$ownership_var)[1]); n_ov0
+p_ov0 = round(as.numeric(table(run$ownership_var)[1])/n_sessions,2); p_ov0
 # 0.25
 as.numeric(names(table(run$ownership_var)[2])) 
-as.numeric(table(run$ownership_var)[2])
-round(as.numeric(table(run$ownership_var)[2])/n_sessions,2)
+n_ov025 = as.numeric(table(run$ownership_var)[2]); n_ov025
+p_ov025 = round(as.numeric(table(run$ownership_var)[2])/n_sessions,2); p_ov025
 # 0.5
 as.numeric(names(table(run$ownership_var)[3])) 
-as.numeric(table(run$ownership_var)[3])
-round(as.numeric(table(run$ownership_var)[3])/n_sessions,2)
+n_ov050 = as.numeric(table(run$ownership_var)[3]); n_ov050
+p_ov050 = round(as.numeric(table(run$ownership_var)[3])/n_sessions,2); p_ov050
 
 ### Numbers of runs extinct per ownership_var:
 extinct_ov = table(run$extinct, run$ownership_var); extinct_ov
 ### Proportions:
 extinct_ov_p = prop.table(extinct_ov,2)
-
 # Reverse row order so extinction is bottom one in plot:
 extinct_ov_p = as.matrix(rbind(extinct_ov_p[2,],extinct_ov_p[1,]))
-# bp = barplot(extinct_ov_p, col = c("#d73027", "#e0f3f8"), space = 0.1, cex.names = 1.25, cex.lab = 1.25, cex.axis = 1.25,
-#         xlab = "Land ownership variability",
-#         ylab = "Proportion extinct/not extinct")
-bp = barplot(extinct_ov_p[1,], col = c("#d73027"), space = 0.1, cex.names = 1.25, cex.lab = 1.25, cex.axis = 1.25, ylim = c(0,0.6),
-        xlab = "Land ownership variability",
-        ylab = "Proportion extinct")
-text(bp[1],y = 0.55, paste("n =",as.numeric(table(run$ownership_var)[1])), cex = 1.25)
-text(bp[2],y = 0.55, paste("n =",as.numeric(table(run$ownership_var)[2])), cex = 1.25)
-text(bp[3],y = 0.55, paste("n =",as.numeric(table(run$ownership_var)[3])), cex = 1.25)
 
-par(mfrow =c(1,3))
-XLAB = "Time step"
-CEX.AXIS = 1.5
-CEX.LAB = 1.5
-CEX.MAIN = 1.5
-CEX.MTEXT = 1.75
-LCOL1 = "#525252"
-LCOL2 = "red"   #d73027
-LWD1 = 1.2
-LWD2 = 1.5
-par(oma= c(5,5,5,0))
-par(mar= c(1,1,1,1))
-plot(gdata_run$t, gdata_run$res, type = "n", ylab = "True population size", xlab = XLAB, cex.axis = CEX.AXIS, cex.lab = CEX.LAB, cex.main = CEX.MAIN)
-mtext("ownership_var = 0", side=3, line =1.5, cex = CEX.MTEXT)
-tapply(gdata_run$res[gdata_run$ownership_var==0 & gdata_run$extinct==0], gdata_run$id[gdata_run$ownership_var==0 & gdata_run$extinct==0], 
-       function(x) lines(x, col = LCOL1, lwd = LWD1))
-tapply(gdata_run$res[gdata_run$ownership_var==0 & gdata_run$extinct==1], gdata_run$id[gdata_run$ownership_var==0 & gdata_run$extinct==1], 
-       function(x) lines(x, col = LCOL2, lwd = LWD2))
-par(mar= c(1,1,1,1))
-plot(gdata_run$t, gdata_run$res, type = "n", yaxt="n", ylab = "", xlab = XLAB, cex.axis = CEX.AXIS, cex.lab = CEX.LAB)
-axis(side = 2, labels = FALSE)
-mtext("ownership_var = 0.25", side=3, line =1.5, cex = CEX.MTEXT)
-tapply(gdata_run$res[gdata_run$ownership_var==0.25 & gdata_run$extinct==0], gdata_run$id[gdata_run$ownership_var==0.25 & gdata_run$extinct==0], 
-       function(x) lines(x, col = LCOL1, lwd = LWD1))
-tapply(gdata_run$res[gdata_run$ownership_var==0.25 & gdata_run$extinct==1], gdata_run$id[gdata_run$ownership_var==0.25 & gdata_run$extinct==1], 
-       function(x) lines(x, col = LCOL2, lwd = LWD2))
-par(mar= c(1,1,1,1))
-plot(gdata_run$t, gdata_run$res, type = "n", yaxt = "n", ylab = "", xlab = XLAB, cex.axis = CEX.AXIS, cex.lab = CEX.LAB)
-axis(side = 2, labels = FALSE)
-mtext("ownership_var = 0.5", side=3, line =1.5, cex = CEX.MTEXT)
-tapply(gdata_run$res[gdata_run$ownership_var==0.5 & gdata_run$extinct==0], gdata_run$id[gdata_run$ownership_var==0.5 & gdata_run$extinct==0], 
-       function(x) lines(x, col = LCOL1, lwd = LWD1))
-tapply(gdata_run$res[gdata_run$ownership_var==0.5 & gdata_run$extinct==1], gdata_run$id[gdata_run$ownership_var==0.5 & gdata_run$extinct==1], 
-       function(x) lines(x, col = LCOL2, lwd = LWD2))
-mtext("Time step", side = 1, outer = T, line = 2.5, cex = CEX.MTEXT)
-mtext("True population size", side = 2, outer = T, line = 2.5, cex =CEX.MTEXT)
-
-### Calculate summary statistic for given cost type over time steps:
-cost_summary = function(dat, ov, cost, type) {
-        
-        if(!(cost %in% names(dat))) stop("'cost' specified not present in 'dat'.")
-        if(!(type %in% c("mean","median","q25","q75","q025","q975"))) stop("invalid 'type' specified .")
-        
-        d = dat[(dat$ownership_var==ov & dat$t>5),c("t",cost)]
-        d[,cost] = (d[,cost]-10)*10
-        d$t = factor(d$t)
-        
-        if(type == "mean") return(tapply(d[,cost],d$t,function(x) mean(x, na.rm=T)))
-        if(type == "median") return(tapply(d[,cost],d$t,function(x) median(x, na.rm=T)))
-        if(type == "q25") return(tapply(d[,cost],d$t,function(x) quantile(x, probs = 0.25, na.rm=T)))
-        if(type == "q75") return(tapply(d[,cost],d$t,function(x) quantile(x, probs = 0.75, na.rm=T)))
-        if(type == "q025") return(tapply(d[,cost],d$t,function(x) quantile(x, probs = 0.025, na.rm=T)))
-        if(type == "q975") return(tapply(d[,cost],d$t,function(x) quantile(x, probs = 0.975, na.rm=T)))
-
-}
-
-par(mfrow =c(2,3))
-XLAB = "Time step"
-CEX.AXIS = 1.5
-CEX.LAB = 1.5
-CEX.MAIN = 1.5
-CEX.MTEXT = 1.75
-LCOL1 = "#525252"
-LCOL2 = "red"   #d73027
-LWD1 = 1.2
-LWD2 = 1.5
-par(oma= c(5,5,5,0))
-par(mar= c(1,1,1,1))
-
-ptype = c(rep("cull_cost",3), rep("scare_cost",3))
-otype = rep(c(0,0.25,0.5),2)
-aytype = rep(c(TRUE,FALSE,FALSE),2)
-axtype = c(rep(FALSE,3),rep(TRUE,3))
-polycols = rbind(cbind(rep("#e6550d",3),rep("#fdae6b",3)),cbind(rep("#a6bddb",3),rep("#2b8cbe",3)))
-
-for(i in 1:6) {
-        plot(c(1,1), xlim = c(1,25), ylim = c(0,1000), 
-             type = "n", ylab = "", xlab = XLAB, cex.axis = CEX.AXIS, cex.lab = CEX.LAB, cex.main = CEX.MAIN, yaxt = "n", xaxt = "n")
-        if(aytype[i]) { axis(2, labels = TRUE, cex.axis = CEX.AXIS) } else { axis(2, labels = FALSE) }
-        if(axtype[i]) { axis(1, labels = TRUE, cex.axis = CEX.AXIS) } else { axis(1, labels = FALSE) }
-        if(i==1) mtext("Hunting cost", side = 2, line = 3, cex = CEX.MTEXT)
-        if(i==4) mtext("Scaring cost", side = 2, line = 3, cex = CEX.MTEXT)
-        
-        if(i<4) { mtext(bquote(o[v] ~ "=" ~ .(otype[i])), side=3, line =1.5, cex = CEX.MTEXT) }
-        polygon(c(rev(6:24),6:24), 
-                c(rev(cost_summary(gdata_run, ov=otype[i], cost=ptype[i], type="q025")[1:19]),
-                  cost_summary(gdata_run, ov=otype[i], cost=ptype[i],type="q975")[1:19]), col = polycols[i,1], border = NA)
-        polygon(c(rev(6:24),6:24), 
-                c(rev(cost_summary(gdata_run, ov = otype[i], cost=ptype[i],type="q25")[1:19]),
-                  cost_summary(gdata_run, ov = otype[i], cost=ptype[i],type="q75")[1:19]), col = polycols[i,2], border = NA)
-        
-        lines(6:24, cost_summary(gdata_run, ov = otype[i], cost=ptype[i],type="median")[1:19], lwd = 2)
-        lines(6:24, cost_summary(gdata_run, ov = otype[i], cost=ptype[i],type="q25")[1:19], lwd = 1)
-        lines(6:24, cost_summary(gdata_run, ov = otype[i], cost=ptype[i],type="q75")[1:19], lwd = 1)
-        lines(6:24, cost_summary(gdata_run, ov = otype[i], cost=ptype[i],type="q025")[1:19], lwd = 0.75)
-        lines(6:24, cost_summary(gdata_run, ov = otype[i], cost=ptype[i],type="q975")[1:19], lwd = 0.75)
-        
-}
-mtext("Time step", side = 1, outer = T, line = 2.5, cex = CEX.MTEXT)
 
